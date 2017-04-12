1a3260150f9615409d1b3f690d412ab759e04e8f
Jarrad Salmon <jbardnz@gmail.com>
2017-04-13 11:05:57 +1200

    Fixed Jaro Winkler

--- src/JaroWinkler.php
+++ src/JaroWinkler.php
@@ -9,86 +9,77 @@ class JaroWinkler
         return $this->JaroWinkler($str1, $str2, $PREFIXSCALE = 0.1);
     }
 
-    private function getCommonCharacters($string1, $string2, $allowedDistance)
-    {
+	private function getCommonCharacters($string1, $string2, $allowedDistance){
 
-        $str1_len = mb_strlen($string1);
-        $str2_len = mb_strlen($string2);
-        $temp_string2 = $string2;
+		$str1_len = strlen($string1);
+		$str2_len = strlen($string2);
+		$temp_string2 = $string2;
 
-        $commonCharacters='';
-        for ($i=0; $i < $str1_len; $i++) {
-            $noMatch = true;
-            // compare if char does match inside given allowedDistance
-            // and if it does add it to commonCharacters
-            for ($j= max(0, $i-$allowedDistance); $noMatch && $j < min($i + $allowedDistance + 1, $str2_len); $j++) {
-                if ($temp_string2[$j] == $string1[$i]) {
-                    $noMatch = false;
-                    $commonCharacters .= $string1[$i];
-                    // original code
-                    // $temp_string2[$j] = '';
-                    //
-                    // following modified for >= 7.0, seems appropriate replacement, but no throws an
-                    // Uninitialized string offset at line 25...
-                    $temp_string2 = substr_replace($temp_string2, '', $j, 1);
-                }
-            }
-        }
-        return $commonCharacters;
-    }
+		$commonCharacters='';
+		for($i=0; $i < $str1_len; $i++){
 
-    private function Jaro($string1, $string2)
-    {
+			$noMatch = True;
+			// compare if char does match inside given allowedDistance
+			// and if it does add it to commonCharacters
+			for($j= max(0, $i-$allowedDistance ); $noMatch && $j < min( $i + $allowedDistance + 1, $str2_len); $j++){
+				if($temp_string2[$j] == $string1[$i]){
+					$noMatch = False;
+					$commonCharacters .= $string1[$i];
+					$temp_string2[$j] = '';
+				}
+			}
+		}
+		return $commonCharacters;
+	}
 
-        $str1_len = mb_strlen($string1);
-        $str2_len = mb_strlen($string2);
+    private function jaro($string1, $string2){
 
-      // theoretical distance
-        $distance = (int) floor(min($str1_len, $str2_len) / 2.0);
+		$str1_len = strlen($string1);
+		$str2_len = strlen($string2);
 
-      // get common characters
-        $commons1 = $this->getCommonCharacters($string1, $string2, $distance);
-        $commons2 = $this->getCommonCharacters($string2, $string1, $distance);
+		// theoretical distance
+		$distance = (int) floor(min($str1_len, $str2_len) / 2.0);
 
-        if (($commons1_len = mb_strlen($commons1)) == 0) {
-            return 0;
-        }
-        if (($commons2_len = mb_strlen($commons2)) == 0) {
-            return 0;
-        }
-      // calculate transpositions
-        $transpositions = 0;
-        $upperBound = min($commons1_len, $commons2_len);
-        for ($i = 0; $i < $upperBound; $i++) {
-            if ($commons1[$i] != $commons2[$i]) {
-                $transpositions++;
-            }
-        }
-        $transpositions /= 2.0;
-      // return the Jaro distance
-        return ($commons1_len/($str1_len) + $commons2_len/($str2_len) + ($commons1_len - $transpositions)/($commons1_len)) / 3.0;
-    }
+		// get common characters
+		$commons1 = $this->getCommonCharacters($string1, $string2, $distance);
+		$commons2 = $this->getCommonCharacters($string2, $string1, $distance);
 
-    private function getPrefixLength($string1, $string2, $MINPREFIXLENGTH = 4)
-    {
+		if(($commons1_len = strlen($commons1)) == 0) return 0;
+		if(($commons2_len = strlen($commons2)) == 0) return 0;
+		// calculate transpositions
+		$transpositions = 0;
+		$upperBound = min($commons1_len, $commons2_len);
+		for($i = 0; $i < $upperBound; $i++){
+			if($commons1[$i] != $commons2[$i]) $transpositions++;
+		}
+		$transpositions /= 2.0;
+		// return the Jaro distance
+		return ($commons1_len/($str1_len) + $commons2_len/($str2_len) + ($commons1_len - $transpositions)/($commons1_len)) / 3.0;
 
-        $n = min(array( $MINPREFIXLENGTH, mb_strlen($string1), mb_strlen($string2) ));
+	}
 
-        for ($i = 0; $i < $n; $i++) {
-            if ($string1[$i] != $string2[$i]) {
-                // return index of first occurrence of different characters
-                return $i;
-            }
-        }
-      // first n characters are the same
-        return $n;
-    }
 
-    private function JaroWinkler($string1, $string2, $PREFIXSCALE = 0.1)
-    {
+	private function getPrefixLength($string1, $string2, $MINPREFIXLENGTH = 4){
 
-        $JaroDistance = $this->Jaro($string1, $string2);
-        $prefixLength = $this->getPrefixLength($string1, $string2);
-        return $JaroDistance + $prefixLength * $PREFIXSCALE * (1.0 - $JaroDistance);
-    }
+		$n = min(array($MINPREFIXLENGTH, strlen($string1), strlen($string2)));
+
+		for($i = 0; $i < $n; $i++){
+			if( $string1[$i] != $string2[$i] ){
+				// return index of first occurrence of different characters
+				return $i;
+			}
+		}
+		// first n characters are the same
+		return $n;
+	}
+
+
+	private function JaroWinkler($string1, $string2, $PREFIXSCALE = 0.1){
+
+		$JaroDistance = $this->jaro($string1, $string2);
+
+		$prefixLength = $this->getPrefixLength($string1, $string2);
+
+		return $JaroDistance + $prefixLength * $PREFIXSCALE * (1.0 - $JaroDistance);
+	}
 }
